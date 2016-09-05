d78a82327673fdf390eee26f6bc1382da27c29d0
Nicolai <nico8829@edu.eal.dk>
2016-09-05 10:02:19 +0200

    solved

---
+++ 00_Hello/Functions.cs
@@ -0,0 +1,11 @@
+﻿namespace _00_Hello
+{
+    public class Functions
+    {
+        public static string Hello(string Fred)
+        {
+            if (Fred == "") return "Hello";
+            return "Hello" + ", " + Fred;
+        }
+    }
+}
